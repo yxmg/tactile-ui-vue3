ab8269901e49f7806358044ec62c95e5479aaacf
yxmg <guanmz@chaosource.com>
2021-02-13 17:50:34 +0800

    feat(switch): 支持切换并加上动画

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -1,10 +1,21 @@
 <template>
-  <button><span></span></button>
+  <button :class="{ checked }" @click="toggleChecked">
+    <span></span>
+  </button>
 </template>
 
 <script lang="ts">
+import {ref} from 'vue'
+
 export default {
-  name: "switch"
+  name: "switch",
+  setup() {
+    const checked = ref(false)
+    const toggleChecked = () => {
+      checked.value = !checked.value
+    }
+    return {checked, toggleChecked}
+  }
 }
 </script>
 
@@ -15,9 +26,10 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: gray;
   border-radius: $h / 2;
   position: relative;
+  outline: none;
 }
 
 span {
@@ -28,9 +40,14 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 0.25s linear;
 }
 
-button:hover > span {
-  left: calc(100% - #{$h2} - 2px);
+button.checked {
+  background-color: blue;
+
+  > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
 }
 </style>
