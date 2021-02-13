6d02cc64716b4c8ee06b2159a5eab430a0c5f284
yxmg <guanmz@chaosource.com>
2021-02-13 20:17:12 +0800

    refactor(switch): 调整样式代码

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -35,6 +35,17 @@ button {
   outline: none;
   transition: background-color 0.25s linear;
 
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $ballHeight;
+    width: $ballHeight;
+    background: white;
+    border-radius: $ballHeight / 2;
+    transition: left 0.25s linear, width 0.25s linear, margin 0.25s linear;
+  }
+
   &:active {
     > span {
       width: $ballHeight + 4px;
@@ -42,7 +53,7 @@ button {
   }
 
   &.checked {
-    background-color: blue;
+    background-color: #1890ff;
 
     &:active {
       > span {
@@ -55,16 +66,5 @@ button {
       left: calc(100% - #{$ballHeight} - 2px);
     }
   }
-}
-
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $ballHeight;
-  width: $ballHeight;
-  background: white;
-  border-radius: $ballHeight / 2;
-  transition: left 0.25s linear, width 0.25s linear, margin 0.25s linear;
 }
 </style>
