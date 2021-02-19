0cf52b138ac341186def20273ba3a029c9ff6728
yxmg <guanmz@chaosource.com>
2021-02-19 20:52:04 +0800

    feat(button): props定义和演示页处理

--- src/lib/button.vue
+++ src/lib/button.vue
@@ -1,61 +1,110 @@
 <template>
   <button
     class="t-button"
-    :class="{ [`t-theme-${theme}`]: theme, }"
+    :class="classes"
   >
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue'
+
 export default {
   name: "Button",
   props: {
+    // 主题，可选值：default、primary、danger、success
     theme: {
       type: String,
       default: 'default'
-    }
+    },
+    // 尺寸，可选值：large、medium、small
+    size: {
+      type: String,
+      default: 'medium'
+    },
+    // 形状，可选：rect、round、circle
+    shape: {
+      type: String,
+      default: 'round'
+    },
+    // 是否禁用
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    // 是否loading
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    // 块状按钮，占满容器
+    block: {
+      type: Boolean,
+      default: false
+    },
+    // 是否链接按钮
+    link: {
+      type: Boolean,
+      default: false
+    },
+    // 幽灵按钮
+    ghost: {
+      type: Boolean,
+      default: false
+    },
+    // TODO：图标，支持内置ICON、RF
+    icon: {}
+  },
+  setup(props) {
+    const {theme, size, shape, link, ghost, loading, block, disabled} = props
+    const classes = computed(() => {
+      return {
+        [`t-theme-${theme}`]: theme,
+        [`t-size-${size}`]: size,
+        [`t-shape-${shape}`]: shape,
+        't-link-button': link,
+        't-ghost-button': ghost,
+        't-loading-button': loading,
+        't-block-button': block,
+        't-disabled-button': disabled,
+      }
+    })
+    return {classes}
   }
 }
 </script>
 
 <style lang="scss">
-$h: 32px;
-$border-color: #d9d9d9;
-$color: #333;
-$blue: #40a9ff;
-$radius: 4px;
 .t-button {
-  box-sizing: border-box;
-  height: $h;
-  padding: 0 12px;
-  cursor: pointer;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
+  display: inline-block;
+  position: relative;
+  line-height: 1.5;
   white-space: nowrap;
-  background: white;
-  color: $color;
-  border: 1px solid $border-color;
-  border-radius: $radius;
-  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  text-align: center;
+  background-image: none;
+  box-shadow: 0 2px 0 rgba(0, 0, 0, .015);
+  cursor: pointer;
+  transition: all .3s cubic-bezier(.645, .045, .355, 1);
+  // 用户选择文本
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  // 只允许滚动和缩放
+  touch-action: manipulation;
+  height: 44px;
+  padding: 0 15px;
+  font-size: 14px;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, .65);
+  background-color: #fff;
+  border: 1px solid #d9d9d9;
 
-  & + & {
-    margin-left: 8px;
-  }
-
-  &:hover,
-  &:focus {
-    color: $blue;
-    border-color: $blue;
-  }
-
+  &,
+  &:active,
   &:focus {
     outline: none;
-  }
-
-  &::-moz-focus-inner {
-    border: 0;
   }
 }
 </style>
--- src/views/button-doc.vue
+++ src/views/button-doc.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>
-    <div>Button示例</div>
-    <h1>示例1</h1>
-    <div>
-      <Button theme="">你好</Button>
+  <div class="button-doc">
+    <h1 style="margin-bottom: 12px;">示例1-theme</h1>
+    <div style="margin-bottom: 24px;">
+      <Button>Default</Button>
+      <Button theme="primary">Primary</Button>
+      <Button theme="danger">Danger</Button>
+      <Button theme="success">Success</Button>
+    </div>
+    <h1 style="margin-bottom: 12px;">示例2-size</h1>
+    <div style="margin-bottom: 24px;">
+      <Button size="large">Large</Button>
+      <Button size="large" link>Large</Button>
+    </div>
+    <div style="margin-bottom: 24px;">
+      <Button>Medium</Button>
+      <Button>Medium</Button>
+    </div>
+    <div style="margin-bottom: 24px;">
+      <Button>Small</Button>
+      <Button>Small</Button>
+    </div>
+    <h1 style="margin-bottom: 12px;">示例3-disabled</h1>
+    <div style="margin-bottom: 24px;">
+      <Button disabled>Default</Button>
+      <Button disabled theme="primary">Primary</Button>
+      <Button disabled theme="danger">Danger</Button>
+      <Button disabled theme="success">Success</Button>
+    </div>
+    <h1 style="margin-bottom: 12px;">示例4-shape</h1>
+    <div style="margin-bottom: 24px;">
+      <Button theme="primary" shape="rect">Rect</Button>
+      <Button theme="primary">Round</Button>
+      <Button theme="primary" shape="circle">Circle</Button>
+    </div>
+    <h1 style="margin-bottom: 12px;">示例5-loading</h1>
+    <div style="margin-bottom: 24px;">
+      <Button theme="primary" shape="rect" loading>Rect</Button>
+      <Button theme="primary" loading>Round</Button>
+      <Button theme="primary" shape="circle" loading>Circle</Button>
+      <Button theme="primary" link loading>Circle</Button>
     </div>
   </div>
 </template>
@@ -13,16 +48,21 @@ import Button from '../lib/button.vue'
 
 export default {
   name: "button-doc",
-  components: {Button},
+  components: { Button },
   setup() {
     const onClick = () => {
       console.log('hi')
     }
-    return {onClick}
+    return { onClick }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.button-doc {
+  .t-button {
+    margin-right: 8px;
+    margin-bottom: 8px;
+  }
+}
 </style>
