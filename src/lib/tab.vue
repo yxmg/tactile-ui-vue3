a60b4cc3b8fdcb08048daeb54c980a4e5205be97
guanmz <15900121620@163.com>
2021-02-22 21:40:50 +0800

    feat: 新增tabs、tab组件

---
+++ src/lib/tab.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Tab组件</div>
+</template>
+
+<script>
+export default {
+  name: "Tab"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/tabs-doc.vue
+++ src/views/tabs-doc.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>Tabs组件</div>
+  <div class="tabs-doc">
+    <h1 style="margin-bottom: 12px;">示例1-基本用法</h1>
+    <div style="margin-bottom: 24px;">
+      <Tabs>
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+      </Tabs>
+    </div>
+  </div>
 </template>
 
 <script>
+import Tabs from '../lib/tabs.vue'
+import Tab from '../lib/tab.vue'
+
 export default {
-  name: "button"
+  name: 'tabs-doc',
+  components: { Tab, Tabs }
 }
 </script>
 
