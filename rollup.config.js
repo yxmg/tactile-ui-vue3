246eb2f319be34b16ac05dbca3b7190b6d8a5c27
guanmz <15900121620@163.com>
2021-02-27 23:36:21 +0800

    feat: 引入rollup打包库

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import esbuild from 'rollup-plugin-esbuild'
+import vue from 'rollup-plugin-vue'
+import scss from 'rollup-plugin-scss'
+import dartSass from 'sass';
+import { terser } from "rollup-plugin-terser"
+
+export default {
+  input: 'src/lib/index.ts',
+  output: [
+    {
+      globals: {
+        vue: 'Vue'
+      },
+      name: 'tinyUI',
+      file: 'dist/lib/tiny-ui.js',
+      format: 'umd',
+      plugins: [terser()]
+    },
+    {
+      name: 'tinyUI',
+      file: 'dist/lib/tinyUI.esm.js',
+      format: 'es',
+      plugins: [terser()]
+    }
+  ],
+  plugins: [
+    scss({ include: /\.scss$/, sass: dartSass }),
+    esbuild({
+      include: /\.[jt]s$/,
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2015'
+    }),
+    vue({ include: /\.vue$/, })
+  ],
+}
---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+/**
+ *Created by 夜雪暮歌 on 2021/2/27
+ **/
+export {default as Switch} from './switch.vue'
+export {default as Button} from './button.vue'
+export {default as Dialog} from './dialog.vue'
+export {default as Tabs} from './tabs.vue'
+export {default as Tab} from './tab.vue'
+export {openDialog} from './open-dialog'
