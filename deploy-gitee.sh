8dd98c5e9c22d128153844eaad8d262a8dee8d24
guanmz <15900121620@163.com>
2021-02-28 15:03:49 +0800

    feat: 添加gitee部署脚本

--- deploy-gitee.sh
+++ deploy-gitee.sh
@@ -5,8 +5,8 @@ git add . &&
 time=$(date "+%Y-%m-%d %H:%M:%S") && git commit -m "feat: 更新-${time}" &&
 git branch -M main &&
 # replace by your website repository
-git remote add origin git@github.com:yxmg/tiny-ui-vue3-website.git &&
+git remote add origin git@gitee.com:yxmg/tiny-ui-vue3-website.git &&
 git push -f origin main &&
 cd - &&
-echo "部署完成，请清除缓存访问"
-start https://yxmg.github.io/tiny-ui-vue3-website/index.html
+echo "部署完成，请手动部署"
+start https://gitee.com/yxmg/tiny-ui-vue3-website/pages
