e8bdd7cca55b07933919b0e3dbd3cdfe4f1822ce
guanmz <15900121620@163.com>
2021-02-28 15:04:45 +0800

    chore: 调整网站部署仓库名

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git add . &&
 time=$(date "+%Y-%m-%d %H:%M:%S") && git commit -m "feat: 更新-${time}" &&
 git branch -M main &&
 # replace by your website repository
-git remote add origin git@github.com:yxmg/tiny-ui-vue3-website.git &&
+git remote add origin git@github.com:yxmg/tiny-ui-vue3-site.git &&
 git push -f origin main &&
 cd - &&
 echo "部署完成，请清除缓存访问"
