8dd915d27772e0bea34861b9a315864a8fbfc988
guanmz <15900121620@163.com>
2021-03-01 23:45:42 +0800

    feat(switch-doc): loading属性演示

---
+++ src/views/switch-doc/components/loading-demo.vue
@@ -0,0 +1,18 @@
+<demo-sign>支持loading</demo-sign>
+<template>
+  <Switch v-model:checked="checked" />
+  <Switch v-model:checked="checked" loading/>
+</template>
+
+<script lang="ts">
+import Switch from '../../../lib/switch.vue'
+import {ref} from 'vue'
+
+export default {
+  components: { Switch },
+  setup() {
+    const checked = ref(false)
+    return { checked }
+  }
+}
+</script>
