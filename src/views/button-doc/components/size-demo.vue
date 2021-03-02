e9db92d930f8d063f9611f6298821a4bf2cc9b77
guanmz <15900121620@163.com>
2021-03-02 12:58:34 +0800

    feat(button-doc): size属性演示

---
+++ src/views/button-doc/components/size-demo.vue
@@ -0,0 +1,32 @@
+<demo-sign>尺寸</demo-sign>
+<template>
+  <Tabs v-model:activeKey="currentTab">
+    <Tab v-for="tab in tabList" :key="tab" :title="tab">
+      <Button :size="tab">default</Button>
+      <Button :size="tab" theme="primary">primary</Button>
+      <Button :size="tab" theme="danger">danger</Button>
+      <Button :size="tab" theme="success">success</Button>
+      <br/>
+      <br/>
+      <Button :size="tab" shape="rect" theme="primary">Rect</Button>
+      <Button :size="tab" shape="round" theme="primary">Round</Button>
+      <Button :size="tab" shape="circle" theme="primary">C</Button>
+    </Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Button from '../../../lib/button.vue'
+import Tabs from '../../../lib/tabs.vue'
+import Tab from '../../../lib/tab.vue'
+import {ref} from 'vue'
+
+export default {
+  components: { Button, Tabs, Tab },
+  setup() {
+    const tabList = ['large', 'normal', 'small']
+    const currentTab = ref('large')
+    return { tabList, currentTab }
+  }
+}
+</script>
--- src/views/button-doc/index.vue
+++ src/views/button-doc/index.vue
@@ -2,18 +2,20 @@
   <div class="button-doc">
     <h1>Button 组件示例 </h1>
     <Demo :component="themeDemo"></Demo>
+    <Demo :component="sizeDemo"></Demo>
   </div>
 </template>
 
 <script lang="ts">
 import Demo from '../../components/Demo.vue'
 import themeDemo from './components/theme-demo.vue'
+import sizeDemo from './components/size-demo.vue'
 
 export default {
   name: "button-doc",
   components: { Demo },
   setup() {
-    return { themeDemo }
+    return { themeDemo, sizeDemo }
   }
 }
 </script>
