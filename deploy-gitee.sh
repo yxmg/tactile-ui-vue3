00daad0364e439680873146670af4fd580273dc1
guanmz <15900121620@163.com>
2021-03-13 12:00:04 +0800

    chore: 项目改名后调整构建脚本

--- deploy-gitee.sh
+++ deploy-gitee.sh
@@ -5,8 +5,8 @@ git add . &&
 time=$(date "+%Y-%m-%d %H:%M:%S") && git commit -m "feat: 更新-${time}" &&
 git branch -M main &&
 # replace by your website repository
-git remote add origin git@gitee.com:yxmg/tiny-ui-vue3-website.git &&
+git remote add origin git@gitee.com:yxmg/tactile-ui-vue3-website.git &&
 git push -f origin main &&
 cd - &&
 echo "部署完成，请手动部署"
-start https://gitee.com/yxmg/tiny-ui-vue3-website/pages
+start https://gitee.com/yxmg/tactile-ui-vue3-website/pages
--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ git add . &&
 time=$(date "+%Y-%m-%d %H:%M:%S") && git commit -m "feat: 更新-${time}" &&
 git branch -M main &&
 # replace by your website repository
-git remote add origin git@github.com:yxmg/tiny-ui-vue3-site.git &&
+git remote add origin git@github.com:yxmg/tactile-ui-vue3-site.git &&
 git push -f origin main &&
 cd - &&
 echo "部署完成，请清除缓存访问"
-start https://yxmg.github.io/tiny-ui-vue3-website/index.html
+start https://yxmg.github.io/tactile-ui-vue3-site/index.html
