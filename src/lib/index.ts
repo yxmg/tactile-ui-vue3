397f7e4b0867893da8d4e3bb1515cceff43309ba
guanmz <15900121620@163.com>
2021-03-13 17:12:52 +0800

    fix: 尝试修复打包后没有transition问题

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 /**
  *Created by 夜雪暮歌 on 2021/2/27
  **/
+import './tactile-ui.scss'
 export {default as Switch} from './switch.vue'
 export {default as Button} from './button.vue'
 export {default as Dialog} from './dialog.vue'
