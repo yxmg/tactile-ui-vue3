d6f6c26535e90199e696a3098e4f54c7caf0b2b6
guanmz <15900121620@163.com>
2021-03-14 10:34:38 +0800

    feat: 首页页面结构搭建

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,140 +1,123 @@
 <template>
-  <div class="page-home">
-    <div class="top-part">
-      <top-nav need-menu-btn/>
-      <div class="banner">
-        <h1 class="title">Tiny UI</h1>
-        <h2 class="desc">The UI library for Vue3 study</h2>
-        <p class="actions">
-          <a class="action-item" href="">Github</a>
-          <router-link class="action-item" to="/doc">开始</router-link>
-        </p>
+  <main class="page-home">
+    <div class="stripes">
+      <div class="stripe s1"></div>
+      <div class="stripe s2"></div>
+      <div class="stripe s3"></div>
+    </div>
+    <section class="content"></section>
+    <div class="road-map">
+      <div class="roads">
+        <div class="surface-roads"></div>
       </div>
     </div>
-    <ul class="features">
-      <li class="feature-item">
-        <svg class="icon">
-          <use xlink:href="#icon-vue"></use>
-        </svg>
-        <h3 class="feature-title">基于 Vue 3</h3>
-        <p class="feature-desc">Vue 3 Composition API</p>
-      </li>
-      <li class="feature-item">
-        <svg class="icon">
-          <use xlink:href="#icon-typescript"></use>
-        </svg>
-        <h3 class="feature-title">基于 TypeScript </h3>
-        <p class="feature-desc">使用 TypeScript</p>
-      </li>
-      <li class="feature-item">
-        <svg class="icon">
-          <use xlink:href="#icon-light"></use>
-        </svg>
-        <h3 class="feature-title">代码易读</h3>
-        <p class="feature-desc">简洁明了的源代码</p>
-      </li>
-    </ul>
-  </div>
+    <div class="triangle">
+      <img src="../assets/triangle.svg" alt="">
+    </div>
+  </main>
 </template>
 
-<script lang="ts">
-import TopNav from '../components/top-nav.vue'
-
+<script>
 export default {
-  name: "home",
-  components: { TopNav }
+  name: "home"
 }
 </script>
 
 <style lang="scss" scoped>
-$green: #02bcc0;
-$border-radius: 4px;
-$font-color: #007974;
-
 .page-home {
+  position: relative;
+  background: linear-gradient(-180deg, #f6bf4e 1%, #f9d281 23%, #fffdf3 71%);
   overflow: hidden;
 
-  .top-part {
-    color: $font-color;
-    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
-    clip-path: ellipse(80% 60% at 50% 40%);
-    text-align: center;
-  }
+  .stripes {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: -80px;
+    transform: skewY(-12deg);
 
-  .banner {
-    padding: 250px 0;
-  }
-
-  .title {
-    margin-bottom: 24px;
-  }
-
-  .desc {
-    margin-bottom: 24px;
-  }
-
-  .action-item {
-    background-color: $green;
-    display: inline-block;
-    color: #fff;
-    padding: 8px 24px;
-    border-radius: $border-radius;
-
-    &:hover {
-      text-decoration: none;
+    > .stripe {
+      position: absolute;
+      height: 190px;
     }
 
-    & ~ .action-item {
-      margin-left: 24px;
+    > .s1 {
+      top: 0;
+      left: 0;
+      width: 30%;
+      background: linear-gradient(90deg, #ffad5a, rgba(250, 175, 82, .25));
+    }
+
+    > .s2 {
+      top: 190px;
+      left: 0;
+      width: 38%;
+      background: linear-gradient(90deg, #ffcd68, rgba(255, 199, 107, 0));
+    }
+
+    > .s3 {
+      top: 380px;
+      right: 0;
+      width: 36%;
+      background: linear-gradient(90deg, hsla(46, 98%, 78%, 0), #ffda84);
     }
   }
 
-  .features {
-    margin: 64px auto;
-    padding: 0 16px;
-    display: flex;
-    flex-wrap: wrap;
+  .road-map {
+    position: relative;
+    width: 100%;
+    height: 500px;
+    perspective: 500px;
+    -webkit-perspective: 500px;
+    -webkit-mask-image: linear-gradient(-180deg, transparent, #000 35%);
+    pointer-events: none;
 
-    .feature-item {
+    .roads {
+      position: absolute;
+      width: calc(100% + 1000px);
+      height: 100%;
+      left: -500px;
+      transform: rotateX(41deg);
+    }
+
+    .surface-roads {
+      position: absolute;
       width: 100%;
-      margin: 16px 0;
-      display: grid;
-      justify-content: start;
-      align-content: space-between;
-      grid-template-areas:
-        "icon title"
-        "icon text";
-      grid-template-columns: 80px auto;
-      grid-template-rows: 1fr auto;
+      height: 1300px;
+      top: -125px;
+      background-image: url('../assets/roads.svg');
+      background-position: 50%;
+      transform: translateY(-650px);
+      animation: roads 10s linear infinite;
+    }
+  }
 
+  .content {
+    position: relative;
+    margin: 190px auto 0;
+    max-width: 1040px;
+    z-index: 1;
+    text-align: center;
+    will-change: transform;
+    width: 100%;
+    padding: 0px 20px;
+  }
 
-      .icon {
-        grid-area: icon;
-        width: 64px;
-        height: 64px;
-      }
+  .triangle {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    bottom: -8px;
+    filter: drop-shadow(rgba(165, 140, 126, 0.15) 0 0 10px);
+  }
 
-      .feature-title {
-        grid-area: title;
-        font-size: 28px;
-      }
-
-      .feature-desc {
-        grid-area: text
-      }
+  @keyframes roads {
+    0% {
+      transform: translateY(-650px)
     }
 
-    @media (min-width: 800px) {
-      width: 800px;
-      .feature-item {
-        width: 50%;
-      }
-    }
-    @media (min-width: 1200px) {
-      width: 1200px;
-      .feature-item {
-        width: 33.3%;
-      }
+    to {
+      transform: translateY(0)
     }
   }
 }
